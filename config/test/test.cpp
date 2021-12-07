09019041231cc8be81242380078e5af4caec1586
SkyTu <1336923451@qq.com>
2021-12-07 11:07:20 +0800

    json related files prepared

---
+++ config/test/test.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: Xinyu Tu
+ * @Date: 2021-12-07 10:04:24
+ * @LastEditors: Xinyu Tu
+ * @LastEditTime: 2021-12-07 10:44:54
+ */
+#include "../GlobalParam.h"
+#include <cstdio>
+#include <fstream>
+#include "json/json.h"
+#include <string.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<B<<endl;
+}   

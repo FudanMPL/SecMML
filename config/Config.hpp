ba38b367ae14135e9fdd4302f6fad4a705d0da79
SkyTu <1336923451@qq.com>
2021-12-09 18:01:10 +0800

    config related method updated

---
+++ config/Config.hpp
@@ -0,0 +1,134 @@
+/*
+ * @Author: Xinyu Tu
+ * @Date: 2021-12-07 10:46:07
+ * @LastEditors: Xinyu Tu
+ * @LastEditTime: 2021-12-09 18:00:13
+ */
+
+#include <cstdio>
+#include <fstream>
+#include "json/json.h"
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+class Config{
+    public:
+        static std::string file_name;
+        static Config* init(){
+            if(config_instance == nullptr){
+                Json::Value root;
+                Json::Reader reader;
+                std::ifstream ifs(file_name);//open file example.json
+                if(!reader.parse(ifs, root)){
+                    std::cout<<"fail to parse"<<std::endl;
+                }
+                config_instance = new Config(root);
+            }
+            return config_instance;
+        }
+        Config(Json::Value root)  : B(root["B"].asInt()), 
+                                    D(root["D"].asInt()),
+                                    PRINT_PRE_ITE(root["PRINT_PRE_ITE"].asInt()),
+                                    OFFLINE_PHASE_ON(root["OFFLINE_PHASE_ON"].asInt()),
+                                    LOCAL_TEST(root["LOCAL_TEST"].asInt()),
+                                    GRAPH_TYPE(root["GRAPH_TYPE"].asInt()),
+                                    ACTIVATION(root["ACTIVATION"].asInt()),
+                                    SIGMOID(root["SIGMOID"].asInt()),
+                                    TANH(root["TANH"].asInt()),
+                                    LINEAR(root["LINEAR"].asInt()),
+                                    LOGISTIC(root["LOGISTIC"].asInt()),
+                                    LEAKEY_RELU_BIAS(root["IE"].asInt()/2),
+                                    MOD(atol(root["MOD"].asString().c_str())),
+                                    N(root["N"].asInt()),
+                                    M(root["M"].asInt()),
+                                    L(root["L"].asInt()),
+                                    D2(root["D"].asInt()/root["L"].asInt()),
+                                    CH(root["CH"].asInt()),
+                                    IE_b(root["IE_b"].asInt()),
+                                    TN(root["TN"].asInt()),
+                                    MAX_NODE_NUM(root["MAX_NODE_NUM"].asInt()),
+                                    MASTER(root["MASTER"].asInt()),
+                                    IE(root["IE"].asInt()),
+                                    NM(root["NM"].asInt()),
+                                    BIT_LENGTH(root["BIT_LENGTH"].asInt()),
+                                    REDUNDANCY(root["REDUNDANCY"].asInt()),
+                                    BIT_P_LEN(root["BIT_P_LEN"].asInt()),
+                                    BUFFER_MAX(root["BUFFER_MAX"].asInt()),
+                                    HEADER_LEN(root["HEADER_LEN"].asInt()),
+                                    ND(root["ND"].asInt()),
+                                    DECIMAL_PLACES(root["DECIMAL_PLACES"].asInt()),
+                                    HEADER_LEN_OPT(root["HEADER_LEN_OPT"].asInt()),
+                                    TRAIN_ITE(root["TRAIN_ITE"].asInt()),
+                                    THREAD_NUM(root["THREAD_NUM"].asInt()),
+                                    MatColMajor(root["MatColMajor"].asInt()),
+                                    MatRowMajor(root["MatRowMajor"].asInt()),
+                                    MM_NN(root["MM_NN"].asInt()),
+                                    MM_NT(root["MM_NT"].asInt()),
+                                    MM_TN(root["MM_TN"].asInt()),
+                                    MM_TT(root["MM_TT"].asInt()),
+                                    NGRAM(root["NGRAM"].asInt()),
+                                    KEY_NUM(root["KEY_NUM"].asInt()),
+                                    KEY_BATCH(root["KEY_BATCH"].asInt()),
+                                    MAX_LEN(root["MAX_LEN"].asInt()),
+                                    INFER_BATCH(root["INFER_BATCH"].asInt()),
+                                    MAX_SMOOTHING_LEVEL(root["MAX_SMOOTHING_LEVEL"].asInt()),
+                                    ALPHABET_SIZE(root["ALPHABET_SIZE"].asInt()),
+                                    CLOCK_MAIN(root["CLOCK_MAIN"].asInt()),
+                                    CLOCK_TRAIN(root["CLOCK_TRAIN"].asInt()),
+                                    FEATURE_DIM(root["FEATURE_DIM"].asInt()){};
+        const int B;
+        const int D;
+        const int PRINT_PRE_ITE;
+        const int OFFLINE_PHASE_ON;
+        const int LOCAL_TEST;
+        const int GRAPH_TYPE;
+        const int ACTIVATION;
+        const int SIGMOID;
+        const int TANH;
+        const int LINEAR;
+        const int LOGISTIC;
+        const int LEAKEY_RELU_BIAS;
+        const long MOD;
+        const int N;
+        const int M;
+        const int L;
+        const int D2;
+        const int CH;
+        const int IE_b;
+        const int TN;
+        const int MAX_NODE_NUM;
+        const int MASTER;
+        const int IE;
+        const int NM;
+        const int BIT_LENGTH;
+        const int REDUNDANCY;
+        const int BIT_P_LEN;
+        const int BUFFER_MAX;
+        const int HEADER_LEN;
+        const int ND;
+        const int DECIMAL_PLACES;
+        const int HEADER_LEN_OPT;
+        const int TRAIN_ITE;
+        const int THREAD_NUM;
+        const int MatColMajor;
+        const int MatRowMajor;
+        const int MM_NN;
+        const int MM_NT;
+        const int MM_TN;
+        const int MM_TT;
+        const int NGRAM;
+        const int KEY_NUM;
+        const int KEY_BATCH;
+        const int MAX_LEN;
+        const int INFER_BATCH;
+        const int MAX_SMOOTHING_LEVEL;
+        const int ALPHABET_SIZE;
+        const int CLOCK_MAIN;
+        const int CLOCK_TRAIN;
+        const int FEATURE_DIM;
+    private:
+        static Config * config_instance;
+};
+Config * Config::config_instance = nullptr;
+std::string Config::file_name = "/home/txy/SecMML/config/parameter/constant.json";
--- config/test/test.cpp
+++ config/test/test.cpp
@@ -2,52 +2,24 @@
  * @Author: Xinyu Tu
  * @Date: 2021-12-07 10:04:24
  * @LastEditors: Xinyu Tu
- * @LastEditTime: 2021-12-09 15:57:20
+ * @LastEditTime: 2021-12-09 17:24:38
  */
 
 #include <cstdio>
 #include <fstream>
 #include "json/json.h"
-#include <string.h>
+#include <string>
 #include <iostream>
+#include <stdlib.h>
+#include <iostream>
+#include "../Config.hpp"
 using namespace std;
-
-class IOconfig{
-    public:
-        static std::string constant_file_path;
-        static std::string party_file_path;
-        static Json::Value init();
-        //解析，new constant
-        class Constant{
-            public:
-                Constant(Json::Value constant_root) : B(constant_root["B"].asInt()){};
-                Constant(int _B) : B(_B){};
-                const int B;
-        };
-};
-
-//init
-std::string IOconfig::constant_file_path = "../parameter/constant.json";
-std::string IOconfig::party_file_path = "../parameter/0.json";
-
-Json::Value IOconfig::init(){
-    Json::Value root;
-    Json::Reader reader;
-    std::ifstream ifs(constant_file_path);//open file example.json
-    if(!reader.parse(ifs, root)){
-        std::cout<<"fail to parse"<<std::endl;
-    }
-    return root;
-}
 
 
 
 int main()
 {
-    Json::Value root;
-    cout<<IOconfig::constant_file_path<<endl;
-    root = IOconfig::init();
-    cout << root["B"] << endl;
-    IOconfig::Constant c(root);
-    cout<<c.B<<endl;
+    Config* config = Config::init();
+    cout<<config->MOD<<endl;
+    cout<<config->LEAKEY_RELU_BIAS<<endl;
 }   

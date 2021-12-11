12350fef8f1e8379d6dd73a3711fdd9cd3bfe06a
SkyTu <1336923451@qq.com>
2021-12-11 14:49:32 +0800

    some annotations added

--- config/Config.cpp
+++ config/Config.cpp
@@ -2,22 +2,26 @@
  * @Author: Xinyu Tu
  * @Date: 2021-12-11 14:41:29
  * @LastEditors: Xinyu Tu
- * @LastEditTime: 2021-12-11 14:45:23
+ * @LastEditTime: 2021-12-11 14:47:15
  */
 #include "Config.hpp"
 
 
 Config * Config::config_instance = nullptr;
+//jsonfile path
 std::string Config::file_name = "/home/txy/SecMML/config/parameter/constant.json";
 
 Config* Config::init(){
     if(Config::config_instance == nullptr){
         Json::Value root;
         Json::Reader reader;
-        std::ifstream ifs(Config::file_name);//open file example.json
+        //open json file
+        std::ifstream ifs(Config::file_name);
         if(!reader.parse(ifs, root)){
             std::cout<<"fail to parse"<<std::endl;
         }
+
+        //Initialize vectors of ip and port
         std::vector<std::string> ip;
         std::vector<int> port;
         int sz = root["IP"].size();
@@ -25,6 +29,7 @@ Config* Config::init(){
             ip.push_back(root["IP"][i].asString().c_str());
             port.push_back(root["PORT"][i].asInt());
         }
+        
         Config::config_instance = new Config(root, ip, port);
     }
     return Config::config_instance;

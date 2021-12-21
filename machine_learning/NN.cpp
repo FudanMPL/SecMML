7eeb6a03656d97c1f1f7a0b54af5855d61e3b262
SkyTu <1336923451@qq.com>
2021-12-21 19:44:13 +0800

    LEARNING RATE updated into Class Config

--- machine_learning/NN.cpp
+++ machine_learning/NN.cpp
@@ -33,7 +33,7 @@ void NN::global_variables_initializer() {
             neuron[i]->initGrad();
         }
         if (neuron[i]->getIsNet()) {
-            neuron[i]->setOpUpdate(new MathOp::Mul_Const_Trunc(getNeuron(i)->getGrad(), getNeuron(i)->getGrad(), 0.0001));
+            neuron[i]->setOpUpdate(new MathOp::Mul_Const_Trunc(getNeuron(i)->getGrad(), getNeuron(i)->getGrad(), Config::config->LEARNING_RATE));
         }
     }
     curForward = 1;
